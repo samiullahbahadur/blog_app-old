1ae2bf4c66123038f2a6e43872858d42c090b120
samiullahbhadur <bahadursamiullah@gmail.com>
2022-06-21 22:43:43 +0430

    Add root and view

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,5 @@
 class UserController < ApplicationController
   def index
   end
+  def show; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'user/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'user#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :user, only: [:index, :show] do
+  
 end
